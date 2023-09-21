30603ae880cbc9def77e0ae20e8c49deb4547c31
Gabriel Nunes <40604481+gabriellnuness@users.noreply.github.com>
2023-09-21 14:03:09 -0300

    feat: weighted least squares fit

--- src/AllanFit.jl
+++ src/AllanFit.jl
@@ -1,68 +1,16 @@
 module AllanFit
 
 
-export greet
+
+using LinearSolve
 
 
 
+export allan_fit
 
-function allan_fit(taus::Array{Float64}, adev::Array{Float64})
-"""
-    Allan noises fitting
 
- Calculate the sensor noise parameters/coefficients from the 
- Allan Variance data using Weighted Linear Regression
 
-  
- Fitted Allan deviation noise values:
-
- C = avar_fit
- unit = measurement unit in SI
-
- Q     --> unit * s
- arw   --> unit * s^(1/2)
- bias  --> unit * s^(0)
- rrw   --> unit * s^(-1/2) 
- rr    --> unit * s^(-1)
-
- ref: 2017_Jerath-Bridging the gap between sensor noise modeling and sensor characterization (https://doi.org/10.1016/j.measurement.2017.09.012 )
-"""
-    
-    ## Weighted Least-Squares Fitting
-    avar = adev.^2
-
-    # The weight is approximately to the order of magnitude
-    weight = 1 ./avar
-    W = diagm(weight)
-    T = [taus.^(-2) taus.^(-1) taus.^(0) taus.^(1) taus.^(2)]
-
-    A = T'*W*T
-    b = T'*W*avar
-
-    prob = LinearProblem(A, b)
-    avar_fit = solve(prob)
-    # avar_fit = A\b
-
-    # Check non trustable values from fit
-    avar_fit_original = avar_fit
-    for i in eachindex(avar_fit) 
-        if avar_fit[i] < 0
-            avar_fit[i] = 0
-            @warn("Noise $i value did not fit correctly")
-        end
-    end
-
-    Q    = (avar_fit[1]/3)^(0.5)    # C = 3*Q^2; Q^2 = (q^2)/12, where q = quantum step size
-    arw  = avar_fit[2]^(0.5)                        # C = N^2
-    bias = (avar_fit[3]^(0.5)) / sqrt(2*log(2)/pi)  # C = (0.6643*B)^2
-    rrw  = (3*avar_fit[4])^(0.5)                    # C = K^2 /3
-    rr   = (2*avar_fit[5])^(0.5)                    # C = R^2 / 2
-
-    avar_fit_original = sqrt.(avar_fit_original'*T')
-    avar_fit_original = avar_fit_original[1,:]
-
-    return (avar_fit_original, Q, arw, bias, rrw, rr)
-end
+include("allan_fit.jl")
 
 
 
---
+++ test/runtests.jl
@@ -0,0 +1,12 @@
+using AllanFit
+using AllanDeviations
+using Test
+using PyPlot
+
+
+@testset "Default format" begin
+
+    @test 1 == 1 # dumb test
+
+
+end
