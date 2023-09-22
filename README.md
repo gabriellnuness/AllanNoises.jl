74c548cd054a164389184c95553095308263f493
Gabriel Nunes <nunesgfs@gmail.com>
2023-09-22 17:19:07 -0300

    doc: example plots and code - added to readme and included svg files

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # AllanNoises.jl
 
 Package to simulate an IMU signal and extract the noise values from an Allan deviation log-log plot.
+
+
+### Example
+``` julia
+using AllanNoises, AllanDeviations
+
+Random.seed!(3)
+# creating signal
+fs = 100.0
+time = 5*60*60
+t = range(start=0, stop=time, step=1/fs)
+n = length(t)
+y = @. 2*randn(n) + cumsum(2e-4*randn(n))   # creating a simulated noisy signal
+adev = allandev(y, fs, frequency=true)
+
+noises = allan_fit(adev.tau, adev.deviation)
+noises_simp = allan_fit_simple(adev.tau, adev.deviation)
+
+```
+
+`allan_fit()` and `allan_fit_simple()` provides the following outputs:
+
+![image](doc/fit1.svg)
+![image](doc/fit2.svg)
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,15 @@ using PyPlot
 using Random
 using Printf
 
-
+custom_plot_colors =   ["#4063d8"
+                        "#9558b2"
+                        "#389826"
+                        "#cb3c33"
+                        "#17becf"
+                        "#e377c2"
+                        "#8c564b"
+                        "#7f7f7f"
+                        "#bcbd22"]
 
 @testset "Default signal" begin
     
@@ -30,26 +38,34 @@ using Printf
 
 
     # Plot fitting
-    close("all");figure(); 
+    close("all");
+    figure(); 
     yscale("log"); xscale("log")
     plot(adev.tau, adev.deviation);
-    plot(adev.tau, noises[1]);
-    noises_string = @sprintf "Q = %.2e\narw = %.2e\ndrift = %.2e\nrrw = %.2e\nrr = %.2e" noises[2] noises[3] noises[4] noises[5] noises[6] 
-    legend(["data",noises_string])
+    plot(adev.tau, noises[1], color=custom_plot_colors[4]);
+        noises_string = @sprintf "Q = %.2e\narw = %.2e\ndrift = %.2e\nrrw = %.2e\nrr = %.2e" noises[2] noises[3] noises[4] noises[5] noises[6] 
+        legend(["data",noises_string])
+        xlabel("Correlation time [s]")
+        ylabel("Signal amplitude [a.u.]")
 
     # Plot simple fitting
-    lineN = noises_simp.arw ./ sqrt.(adev.tau)
-    lineB = noises_simp.bias * 0.664 * ones(size(adev.tau));
-    lineK = noises_simp.rrw .* sqrt.(adev.tau/3);
+    line_arw = noises_simp.arw ./ sqrt.(adev.tau)
+    line_bias = noises_simp.bias * 0.664 * ones(size(adev.tau));
+    line_rrw = noises_simp.rrw .* sqrt.(adev.tau/3);
     figure()
-    loglog(adev.tau, adev.deviation, color="tab:gray")
-    loglog(adev.tau, lineN,"--",color="tab:blue",linewidth=.8)
-    loglog(1,noises_simp.arw,"o",color="tab:blue")
-    loglog(adev.tau, lineB,"--",color="tab:purple",linewidth=.8)
-    loglog(adev.tau[noises_simp.ind_bias], noises_simp.bias*0.664,"o",color="tab:purple")
-    loglog(adev.tau, lineK,"--",color="tab:green",linewidth=.8)
-    loglog(3,noises_simp.rrw,"o",color="tab:green")
-    noises_string = @sprintf "arw = %.2e\ndrift = %.2e\nrrw = %.2e" noises_simp.arw noises_simp.bias noises_simp.rrw
-    legend(["data",noises_string])
+    loglog(adev.tau, adev.deviation)
+    loglog(adev.tau, line_arw,"--", color=custom_plot_colors[2],linewidth=.8, label="_nolegend_")
+    loglog(1, noises_simp.arw,"o",markersize=3,color=custom_plot_colors[2])
+    loglog(adev.tau, line_bias,"--",color=custom_plot_colors[3],linewidth=.8, label="_nolegend_")
+    loglog(adev.tau[noises_simp.ind_bias], noises_simp.bias*0.664,"o", markersize=3, color=custom_plot_colors[3])
+    loglog(adev.tau, line_rrw,"--",color=custom_plot_colors[4],linewidth=.8, label="_nolegend_")
+    loglog(3, noises_simp.rrw,"o",markersize=3,color=custom_plot_colors[4])
+        str_arw = @sprintf "arw = %.2e" noises_simp.arw
+        str_bias = @sprintf "drift = %.2e" noises_simp.bias
+        str_rrw = @sprintf "rrw = %.2e" noises_simp.rrw
+        legend(["data",str_arw,str_bias,str_rrw])
+        xlabel("Correlation time [s]")
+        ylabel("Signal amplitude [a.u.]")
+
 
 end
